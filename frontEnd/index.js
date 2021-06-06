367d9dd579f614cfb74279d95467ff34ae9f3d2d
Dhruv <dhruvrawat2611@gmail.com>
2021-06-06 12:13:38 +0530

    Worked on CSS

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -3,7 +3,7 @@ var request = new XMLHttpRequest()
 function solve() {
     var s = document.getElementById('movie').value;
     console.log(s);
-    let url='http://127.0.0.1:3000?id=Deadpool';
+    let url='http://127.0.0.1:3000?id='+s;
     request.open('GET',url,true);
     request.onload = function () {
         var data = this.response;
